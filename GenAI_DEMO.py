b56684e5b956becdab0149f787d6fcceda2cccdd
Ninikiki523 <167546243+Ninikiki523@users.noreply.github.com>
2024-05-28 11:31:40 +0200

    Update GenAI_DEMO.py

--- GenAI_DEMO.py
+++ GenAI_DEMO.py
@@ -6,7 +6,7 @@ import pandas as pd
 # Snowflake parameter
 conn_params = {
     "user": "zhemma523",
-    "password": "Mayday_523",
+    "password": "Exxeta_123456",
     "account": "CHPVMZF-CH77012",
     "warehouse": "COMPUTE_WH"
     #"database": "SNOWFLAKE_SAMPLE_DATA",
