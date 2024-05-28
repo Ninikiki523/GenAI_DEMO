5446082abfe07a6d7b7b27047835bd424ae0c4df
Ninikiki523 <167546243+Ninikiki523@users.noreply.github.com>
2024-05-28 10:50:46 +0200

    Update GenAI_DEMO.py

--- GenAI_DEMO.py
+++ GenAI_DEMO.py
@@ -100,7 +100,7 @@ if st.button('send'):
             columns = [col[0] for col in cs.description]
             results = pd.DataFrame(rows, columns=columns)
             #show query result
-            st.dataframe(results)
+            st.dataframe(results，index=False)
             
         else:
             st.write("No results found.")
