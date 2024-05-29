9fe747d85a7e0f1b05f1e5d0f4dc61f1891c9c16
Ninikiki523 <167546243+Ninikiki523@users.noreply.github.com>
2024-05-29 11:33:47 +0200

    Update GenAI_DEMO.py

--- GenAI_DEMO.py
+++ GenAI_DEMO.py
@@ -18,6 +18,8 @@ ctx = snowflake.connector.connect(**conn_params)
 cs = ctx.cursor(DictCursor)
 
 # title
+exxeta_icon_html = "<img src='https://cdn-assets-eu.frontify.com/s3/frontify-enterprise-files-eu/eyJwYXRoIjoiZXh4ZXRhXC9hY2NvdW50c1wvMWVcLzQwMDExMThcL3Byb2plY3RzXC8zXC9hc3NldHNcL2YyXC8zMjRcL2I0MzA3NjI4Nzc1YmQxZTJhMGY1MDQ2NTA0MTE5MmMzLTE2MzM2MTMwNTkucG5nIn0:exxeta:wrfspUh9DuITqM5FZdxNHaLLCguddQ1Og_lF-P6LM4g?width=2400' class='icon' style='height:24px; width:24px;' />"
+st.markdown(f"{exxeta_icon_html}", unsafe_allow_html=True)
 gradient_text_html = """
 <style>
 .gradient-text {
